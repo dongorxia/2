a7d3f288225f853ddfb41c639714899cd1a47396
dongorxia <134200733+dongorxia@users.noreply.github.com>
2023-05-21 22:26:25 +0800

    Add files via upload

---
+++ js/skystar.js
@@ -0,0 +1,61 @@
+// poem
+var words=[
+    '伤心桥下春波绿',
+    '曾是惊鸿照影来',
+    '当年明月在',
+    '曾照彩云归',
+    '归去来兮',
+    '真堪偕隐',
+    '画船听雨眠',
+];
+function randomNum(min,max){
+    var num = (Math.random()*(max-min+1)+min).toFixed(2);
+    return num;
+}
+function init(){
+    let container = document.querySelector('.container');
+    let f = document.createDocumentFragment();
+    words.forEach(w=>{
+    let word_box = document.createElement('div');
+    let word = document.createElement('div');
+        word.innerText = w;
+        word.classList.add('word');
+        word.style.color = '#BAABDA';
+        word.style.fontFamily = '楷体';
+        word.style.fontSize = '20px'
+        word_box.classList.add('word-box');
+        word_box.style.setProperty("--margin-top",randomNum(-40,20)+'vh');
+        word_box.style.setProperty("--margin-left",randomNum(6,35)+'vw');
+        word_box.style.setProperty("--animation-duration",randomNum(8,20)+'s');
+        word_box.style.setProperty("--animation-delay",randomNum(-20,0)+'s');
+        
+        word_box.appendChild(word);
+        f.appendChild(word_box);
+
+
+    })
+    container.appendChild(f);
+}
+window.addEventListener('load',init);
+let textone = document.querySelector('.textone').querySelector('h1');
+      let texttwo = document.querySelector('.texttwo').querySelector('h1');
+      let textthree = document.querySelector('.textthree').querySelector('h1');
+
+      setTimeout(function(){
+        textone.innerHTML = '今晚，整片星空将为你一人闪烁';
+          textone.style.color = '#E8F9FD';
+          textone.style.fontFamily = '楷体'
+          texttwo.style.color = '#E8F9FD';
+          texttwo.style.fontFamily = '楷体'
+          textthree.style.color = '#E8F9FD';
+          textthree.style.fontFamily = '楷体'
+          texttwo.innerHTML = '';
+      },28000)
+      setTimeout(function(){
+        textone.innerHTML = '只愿意凭着灵感的相通，时时带给彼此以慰藉';
+        texttwo.innerHTML = '像流星的光辉，照耀我疲惫的梦寐';
+        textthree.innerHTML = '永远存一个安慰，纵然在别离的时候';
+      },112500)
+
+
+ 
